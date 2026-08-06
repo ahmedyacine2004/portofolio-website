171a92d573202030be65c7b7c99ade6da839f792
Ahmed Yassine Abbane <ahmedyacineabbane@gmail.com>
2026-08-06 18:23:11 +0100

    chore: configure security middleware

--- apps/server/src/config/app.config.ts
+++ apps/server/src/config/app.config.ts
@@ -3,5 +3,7 @@ export default () => ({
     name: process.env.APP_NAME,
     port: parseInt(process.env.PORT ?? '5000', 10),
     environment: process.env.NODE_ENV,
+
+    frontendUrl: process.env.CORS_ORIGIN,
   },
 });
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,6 +1,9 @@
+import helmet from 'helmet';
+
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { setupSwagger } from './common/swagger/swagger.config';
@@ -9,7 +12,46 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+
   const port = configService.getOrThrow<number>('app.port');
+
+  const frontendUrl = configService.getOrThrow<string>('app.frontendUrl');
+
+  const httpAdapter = app.getHttpAdapter();
+
+  /*
+   |--------------------------------------------------------------------------
+   | Security Headers
+   |--------------------------------------------------------------------------
+   */
+
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: false,
+    }),
+  );
+
+  /*
+   |--------------------------------------------------------------------------
+   | CORS
+   |--------------------------------------------------------------------------
+   */
+
+  app.enableCors({
+    origin: [frontendUrl],
+
+    credentials: true,
+
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  /*
+   |--------------------------------------------------------------------------
+   | Global Pipes
+   |--------------------------------------------------------------------------
+   */
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -22,13 +64,27 @@ async function bootstrap(): Promise<void> {
     }),
   );
 
+  /*
+   |--------------------------------------------------------------------------
+   | Global Filters
+   |--------------------------------------------------------------------------
+   */
+
   app.useGlobalFilters(new GlobalExceptionFilter());
 
+  /*
+   |--------------------------------------------------------------------------
+   | Swagger
+   |--------------------------------------------------------------------------
+   */
+
   setupSwagger(app);
+
+  httpAdapter.getInstance().disable('x-powered-by');
+  httpAdapter.getInstance().set('trust proxy', 1);
 
   await app.listen(port);
 
   console.log(`🚀 Server running on http://localhost:${port}`);
 }
-
 void bootstrap();
